0b7286ddb5b85d3630d9824975f14a9f60d4b530
ant-tr-v <trusov.anton.02@gmail.com>
2018-12-14 23:49:27 +0300

    Numerov f calculation

--- def-eq.py
+++ def-eq.py
@@ -32,35 +32,20 @@ class ThreeDiagonalEq:
         A1 = self.A.copy()
         F1 = self.F.copy()
 
-        N = len(self.A)
-        mat = np.zeros((N, N))
-        for i in range(N - 1):
-            mat[i][i] = self.A[i]
-            mat[i][i+1] = self.B[i]
-            mat[i +1][i] = self.C[i]
-        mat[-1][-1] = self.A[-1]
-        print(mat)
-
-        xx = np.linalg.solve(mat, self.F)
-
         for i in range(1, self.size):
             A1[i] -= self.C[i - 1] * self.B[i - 1] / A1[i - 1]
-            F1[i] -= self.C[i - 1] * F1[i-1] / A1[i - 1]
+            F1[i] -= self.C[i - 1] * F1[i - 1] / A1[i - 1]
         F1[-1] /= A1[-1]
-
-        mat2 = np.zeros((N, N))
-        for i in range(N - 1):
-            mat2[i][i] = A1[i]
-            mat2[i][i + 1] = self.B[i]
-        mat2[-1][-1] = A1[-1]
-        print(mat2)
         for i in range(2, self.size + 1):
             F1[-i] = (F1[-i] - self.B[-i + 1] * F1[-i + 1]) / A1[-i]
         return F1
 
 
-def calculate_F(x: np.ndarray):
-    return np.sin(x)
+def calculate_F(f):
+    res = f.copy()
+    for i in range(1, len(res) - 1):
+        res[i] += (f[i + 1] - 2 * f[i] + f[i - 1]) / 12
+    return res
 
 
 def calculate_ABC(h, size):
@@ -76,7 +61,7 @@ def calculate_ABC(h, size):
 
 
 def solution(x):
-    return x * math.sin(1) - np.sin(x)  # (x - 10)*e10/20 + np.exp(-x) + (x + 10)/(20*e10) + 2
+    return -x ** 4 / 1200 + 1./10 * x *(5 + math.sin(10)) -  np.sin(x) + 40./3
 
 
 def d2(y, h):
@@ -85,27 +70,24 @@ def d2(y, h):
         y0[i - 1] = (y[i - 1] - 2 * y[i] + y[i + 1]) / (h * h)
     return y0
 
+
 N = 25
-x = np.linspace(-1, 1, N)
-x1 = np.linspace(-1, 1, 1000)
+a = -10
+b = 10
+x = np.linspace(a, b, N)
+h = x[1] - x[0]
+x1 = np.linspace(a, b, 1000)
 
 tr = ThreeDiagonalEq(N)
-tr.F = calculate_F(x)
-tr.A, tr.B, tr.C = calculate_ABC(x[1] - x[0], N)
+tr.F = calculate_F(np.sin(x) - x * x / 100)
+tr.A, tr.B, tr.C = calculate_ABC(h, N)
 tr.A[0] = tr.A[-1] = 1
-tr.F[0] = tr.F[-1] = 0
+tr.F[0] = 0
+tr.F[-1] = 10
 
 y0 = tr.solve()
-
-print(tr.B)
-print(tr.A)
-print(tr.C)
 
 plt.plot(x1, solution(x1))
 plt.plot()
 plt.plot(x, y0)
-plt.show()
-
-plt.plot(x[1:-1], tr.F[1:-1])
-plt.plot(x[1:-1], d2(y0, x[1] - x[0]))
 plt.show()
